6001328b478b1f860b747c37b6a89519795e9754
LMD <23167274+lmd-code@users.noreply.github.com>
2022-05-31 13:50:45 +0100

    Refactored to use Map inatead of Object

--- LmdStore.js
+++ LmdStore.js
@@ -1,152 +1,166 @@
-'use strict';
-
 /**
- * Wrapper library for interacting with localStorage on an individual store level
- * 
- * @todo Refactor to use Map instead of object
- * https://stackoverflow.com/questions/29085197/how-do-you-json-stringify-an-es6-map
+ * LmdStore - wrapper for interacting with localStorage
+ * @author LMD-Code
+ * @version 1.0.0
  */
-class LmdStore {
-    /**
-     * Initialise a store
-     * @param {string} storeKey localStorage item key
-     */
-    constructor(storeKey) {
-        /** @property {string} storeName Name (key) of localStorage item */
-        this.storeName = storeKey;
-        
-        /** @property {object} data Data items in key/value pairs */
-        this.data = this.getStore(storeKey);
-    }
 
-    /**
-     * Number of items in store
-     */
-    get length() {
-        if (this.data === null) return 0;
-        return Object.keys(this.data).length;
-    }
-    
-    /**
-     * Get localStorage item, convert resulting JSON string into object and return
-     * 
-     * @returns object
-     */
-    getStore() {
-        let jsonStr = localStorage.getItem(this.storeName);
-        
-        if (jsonStr) {
-            let obj = JSON.parse(jsonStr);
-        
-            if (obj) {
-                return obj;
-            }
-        }
-        
-        return {};
-    }
-    
-    /**
-     * Set localStorage item (creates/overwrites) after converting object back into JSON string
-     */
-    setStore() {
-        let jsonStr = JSON.stringify(this.data);
-        
-        if (jsonStr) {
-            localStorage.setItem(this.storeName, jsonStr);
-        }
-    }
-    
-    /**
-     * Get value of individual data item by key
-     * 
-     * @param {string} key Stored data item key (object property)
-     * @returns mixed|null
-     */
-    getItem(key) {
-        if (this.data.hasOwnProperty(key)) {
-            return this.data[key];
-        }
-        return null;
-    }
-    
-    /**
-     * Get all items at once.
-     * 
-     * Optionally return as a Map object instead of regular object.
-     * 
-     * @param {Boolean} asMap Set to `true` to return Map object (default `false`)
-     */
-    getItems(asMap) {
-        if (!asMap) {
-            return this.data; // the original data object
-        }
-        
-        let data = new Map();
-        for (const key in this.data) {
-            if (this.data.hasOwnProperty(key)) {
-                data.set(key, this.data[key]);
-            }
-        }
-        return data; // iterable Map object
-    }
-    
-    /**
-     * Set value of individual data item by key
-     * 
-     * @param {string} key 
-     * @param {mixed} val
-     */
-    setItem(key, val) {
-        this.data[key] = val;
-        this.setStore();
-    }
-    
-    /**
-     * Set values of multiple data items
-     * 
-     * @param {object} objData 
-     */
-    setItems(objData) {
-        for (const prop in objData) {
-            if (objData.hasOwnProperty(prop)) {
-                this.data[prop] = objData[prop];
-            }
-        }
-        this.setStore();
-    }
+ 'use strict';
 
-    /**
-     * Remove individual data item by key
-     * 
-     * @param {string} key 
-     */
-    removeItem(key) {
-        if (this.data.hasOwnProperty(key)) {
-            delete this.data[key];
-        }
-        this.setStore();
-    }
-
-    /**
-     * Remove multiple data items by key
-     * 
-     * @param {array} keys 
-     */
-    removeItems(keys) {
-        for (let i = 0; i < keys.length; i++) {
-            let key = keys[i];
-            if (this.data.hasOwnProperty(key)) {
-                delete this.data[key];
-            }
-        }
-        this.setStore();
-    }
-
-    /**
-     * Clear (remove) data store
-     */
-    clearAll() {
-        localStorage.clear(this.storeName);
-    }
-}
+ /**
+  * Wrapper class for interacting with localStorage on an individual store level
+  */
+ class LmdStore {
+     /**
+      * Initialise a store
+      * @param {string} storeKey - localStorage item key
+      * @param {string} mapKey   - (Optional) Key for identifying Maps when data is stringified, 
+      *                            helps with converting to and from JSON
+      */
+     constructor(storeKey, mapKey = '_map') {
+         /** @property {string} storeName - Name (key) of localStorage item */
+         this.storeName = storeKey;
+         
+         /** @property {string} mapKey - Key for identifying Map objects when stringified as JSON */
+         this.mapKey = mapKey;
+ 
+         /** @property {Map} data - Data items in key/value pairs */
+         this.data = this.getStore(storeKey);
+     }
+ 
+     /**
+      * Number of stored items
+      * @return {number} Number of items
+      */
+     get count() {
+         if (this.data === null) return 0;
+         return this.data.size;
+     }
+     
+     /**
+      * Get localStorage item, then convert JSON string into Map
+      * @return {Map} Stored data
+      */
+     getStore() {
+         let jsonStr = localStorage.getItem(this.storeName);
+         
+         if (jsonStr !== null) {
+             try {
+                 let data = JSON.parse(jsonStr, (key, value) => {
+                     if (typeof value === 'object' && value !== null) {
+                         if (value.hasOwnProperty(this.mapKey)) return new Map(value[this.mapKey]);
+                     }
+                     return value;
+                 });
+ 
+                 return data;
+             } catch (e) {
+                 console.error(e.message);
+             }
+         }
+         
+         return new Map();
+     }
+     
+     /**
+      * Set localStorage item after converting Map back into JSON string
+      */
+     setStore() {
+         try {
+             let jsonStr = JSON.stringify(this.data, (key, value) => {
+                 if(value instanceof Map) return {[this.mapKey]: Array.from(value.entries())};
+                 return value;
+             });
+             
+             if (typeof jsonStr !== 'undefined') {
+                 localStorage.setItem(this.storeName, jsonStr);
+             }
+         } catch (e) {
+             console.error(e.message);
+         }
+     }
+     
+     /**
+      * Get value of individual data item by key
+      * @param {string} key Stored data item key
+      * @returns {*} Item value or null
+      */
+     getItem(key) {
+         if (this.data.has(key)) {
+             return this.data.get(key);
+         }
+         return null;
+     }
+     
+     /**
+      * Get entire Map object
+      * @return {Map} Stored data
+      */
+     getItems() {
+         return this.data;
+     }
+     
+     /**
+      * Set value of individual data item by key
+      * @param {string} key Item key
+      * @param {*} val Item value
+      */
+     setItem(key, val) {
+         this.data.set(key, val);
+         this.setStore();
+     }
+     
+     /**
+      * Set values of multiple data items
+      * @param {Object} objData Item key/value pairs
+      */
+     setItems(objData) {
+         let setCount = 0;
+ 
+         for (const prop in objData) {
+             if (objData.hasOwnProperty(prop)) {
+                 this.data.set(prop, objData[prop]);
+                 setCount++;
+             }
+         }
+ 
+         if (setCount > 0) {
+             this.setStore(); // only save if item has been added
+         }
+     }
+ 
+     /**
+      * Remove individual data item by key
+      * @param {string} key Item key
+      */
+     removeItem(key) {
+         if (this.data.delete(key)) {
+             this.setStore(); // only save if successful
+         }
+     }
+ 
+     /**
+      * Remove multiple data items by key
+      * @param {array} keys Item keys
+      */
+     removeItems(keys) {
+         let delCount = 0;
+ 
+         for (let i = 0; i < keys.length; i++) {
+             if (this.data.delete(keys[i])) {
+                 delCount++;
+             }
+         }
+ 
+         if (delCount > 0) {
+             this.setStore(); // only save if item has been deleted
+         }
+     }
+ 
+     /**
+      * Clear (remove) data store
+      */
+     clearAll() {
+         this.data.clear(); // clear Map
+         localStorage.clear(this.storeName); // remove localStore entry
+     }
+ }
